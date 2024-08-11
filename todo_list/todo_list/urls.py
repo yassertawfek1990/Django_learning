1520bb57cbf9bb0a510d17ee316665ac04e4212d
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-08-11 09:24:05 +0300

    created registeration forms and connect the data entered to a userprofile table

--- todo_list/todo_list/urls.py
+++ todo_list/todo_list/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("todo.urls"))
+    path("", include("todo.urls")),
+    path('users/', include('users.urls')),
+    path('members/', include('users.urls'))
 ]
