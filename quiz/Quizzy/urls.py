3fd0726773031c91937b2b721b0d5c8a51d9e91c
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-08-15 16:32:23 +0300

    added links to the homepage layout and added the random view almost done just some final touches

--- quiz/Quizzy/urls.py
+++ quiz/Quizzy/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("all-categories", views.categories, name="all_categories"),
-    path("play", views.quiz_view, name="game"),
+    path("all-categories/<slug:level>", views.categories, name="all_categories"),
+    path("play/<slug:level>/<int:cat>", views.quiz_view, name="game"),
     path("results", views.resulting, name="result"),
+    path("r", views.random, name="random"),
 ]
--- quiz/Quizzy/views.py
+++ quiz/Quizzy/views.py
@@ -1,17 +1,18 @@
+from random import choice, shuffle
 from django.shortcuts import render,redirect
 from users.models import UserProfile
 import requests
-import random
+
 # Create your views here.
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 
 
-def get_questions():
+def get_questions(level, cat):
     parameters = {
         "amount" : 10,
-        "category":9, #general knowledge
-        "difficulty":"easy",
+        "category":cat, #general knowledge
+        "difficulty":level,
         "type": "multiple"
     }
     response = requests.get("https://opentdb.com/api.php",params= parameters)
@@ -20,7 +21,7 @@ def get_questions():
     return question_data
 
 bank = {}
-def quiz_view(request):
+def quiz_view(request,level,cat):
     global n
     global questions
     global score
@@ -39,25 +40,26 @@ def quiz_view(request):
             answer = question["correct_answer"]
             wrong = question["incorrect_answers"]
             wrong.append(answer)
-            random.shuffle(wrong)
-            return render(request, 'Quizzy/test.html', {"question": q, "options": wrong, "n":n})
+            shuffle(wrong)
+            return render(request, 'Quizzy/test.html', {"question": q, "options": wrong, "n":n,'level': level,'cat': cat})
         else:
         # You can now use `selected_answer` for whatever you need, like saving it to a database, processing it, etc.
             return redirect("result")
     else:
         n = 0
         score = 0
-        questions = get_questions()
+        questions = get_questions(level,cat)
         question = questions[n]
         q = question["question"]
         answer = question["correct_answer"]
         wrong = question["incorrect_answers"]
         wrong.append(answer)
-        random.shuffle(wrong)
-        return render(request, 'Quizzy/test.html', {"question": q, "options": wrong,"n":n})
+        shuffle(wrong)
+        return render(request, 'Quizzy/test.html', {"question": q, "options": wrong,"n":n,'level': level,'cat': cat})
 
-def categories(request):
-    return render(request,'Quizzy/categories.html')
+def categories(request,level):
+    all_categories = {"Vehicles": 28,"Animals":27,"celebrities":26,"Art":25,"Politics":24,"History":23,"Geography":22, "Sports":21, "General Knowledge":9}
+    return render(request,'Quizzy/categories.html',{"level":level, "cats":all_categories})
 
 def play(request):
     return render(request,'Quizzy/quizpage.html')
@@ -68,11 +70,18 @@ def resulting(request):
     data = []
     for item in questions:
         data.append([item["question"],item["correct_answer"]])
-        # all_questions.append(item["question"])
-        # correct_answers.append(item["correct_answer"])
+  
 
     if request.user.is_authenticated: 
         user_profile = UserProfile.objects.get(user=request.user)  # Get the UserProfile of the logged-in user
         user_profile.total_score += score  # Add points to the total_score
         user_profile.save()  # Save the updated profile
+    else:
+        user_profile = None
     return render(request,'Quizzy/result.html',{"all":data, "the_score":score, "info": user_profile })
+
+
+def random(request):
+    random_category = choice([9,21,22,23,24,25,26,27,28])
+    random_level = choice(["easy","medium","hard"])
+    return redirect('game', level=random_level, cat=random_category)
