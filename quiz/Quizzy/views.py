c4a3e39b33378e9c5303bf27b8fca8c7fb9f664e
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-08-21 15:41:19 +0300

    added a sending email feature when ranking improved and added a play again button in the results section to play same category same level

--- quiz/Quizzy/views.py
+++ quiz/Quizzy/views.py
@@ -2,6 +2,7 @@ from random import choice, shuffle
 from django.shortcuts import render,redirect
 from users.models import UserProfile
 import requests
+from django.core.mail import send_mail
 
 # Create your views here.
 from django.http import HttpResponseRedirect
@@ -25,7 +26,10 @@ def quiz_view(request,level,cat):
     global n
     global questions
     global score
-
+    global same_level
+    global same_category
+    same_level = level
+    same_category = cat
     if request.method == "POST":
         selected_answer = request.POST.get('q_answer')
         print(selected_answer)
@@ -41,9 +45,10 @@ def quiz_view(request,level,cat):
             wrong = question["incorrect_answers"]
             wrong.append(answer)
             shuffle(wrong)
-            return render(request, 'Quizzy/test.html', {"question": q, "options": wrong, "n":n,'level': level,'cat': cat})
+            return render(request, 'Quizzy/test.html', {"question": q, "options": wrong, "n":n+1,'level': level,'cat': cat})
         else:
         # You can now use `selected_answer` for whatever you need, like saving it to a database, processing it, etc.
+            
             return redirect("result")
     else:
         n = 0
@@ -55,7 +60,7 @@ def quiz_view(request,level,cat):
         wrong = question["incorrect_answers"]
         wrong.append(answer)
         shuffle(wrong)
-        return render(request, 'Quizzy/test.html', {"question": q, "options": wrong,"n":n,'level': level,'cat': cat})
+        return render(request, 'Quizzy/test.html', {"question": q, "options": wrong,"n":n+1,'level': level,'cat': cat})
 
 def categories(request,level):
     all_categories = {"Vehicles": 28,"Animals":27,"celebrities":26,"Art":25,"Politics":24,"History":23,"Geography":22, "Sports":21, "General Knowledge":9}
@@ -72,13 +77,31 @@ def resulting(request):
         data.append([item["question"],item["correct_answer"]])
   
 
-    if request.user.is_authenticated: 
-        user_profile = UserProfile.objects.get(user=request.user)  # Get the UserProfile of the logged-in user
+    if request.user.is_authenticated:
+        old_queryset = list(UserProfile.objects.order_by('-total_score'))
+        print(old_queryset)
+        user_profile = UserProfile.objects.get(user=request.user.id)  # Get the UserProfile of the logged-in user
+        print(user_profile)
+        old_rank = old_queryset.index(user_profile) + 1
+        print(old_rank)
         user_profile.total_score += score  # Add points to the total_score
         user_profile.save()  # Save the updated profile
+        new_queryset = list(UserProfile.objects.order_by('-total_score'))
+        print(new_queryset)
+        new_rank = new_queryset.index(user_profile) + 1
+        print(new_rank)
+        if new_rank < old_rank:
+            send_mail(
+                        "Your Ranking improved",
+                        f"Congratulations you have improved your ranking\n your old ranking was {old_rank} and your new ranking is {new_rank}",
+                        "fokak908070@gmail.com",
+                        [user_profile.user.email],
+                        fail_silently=False,)
     else:
         user_profile = None
-    return render(request,'Quizzy/result.html',{"all":data, "the_score":score, "info": user_profile })
+        old_rank = None
+        new_rank = None
+    return render(request,'Quizzy/result.html',{"all":data, "the_score":score, "info": user_profile, "old":old_rank ,"new":new_rank, "level": same_level, "category": same_category })
 
 
 def random(request):
